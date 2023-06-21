1df82cfbcab7e02976ddfe60efcd032e45df7022
Dovhun Artem <dovhun3d@gmail.com>
2023-06-21 17:06:14 +0300

    Create app

---
+++ src/components/Nav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {}
+
+const Nav = () => {
+  return <div></div>
+}
+
+export default Nav
---
+++ src/pages/new/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const New = () => (
+  <div className="w-fit container mx-auto">
+    <h1>new page</h1>
+  </div>
+);
+
+export default New;
