160f97fbb9c8a8070ee48ec5365a4a60736c57a2
Dovhun Artem <dovhun3d@gmail.com>
2023-06-21 18:31:22 +0300

    Fix linter errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,3 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import Nav from '@/components/Nav';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
   return (
     <div
